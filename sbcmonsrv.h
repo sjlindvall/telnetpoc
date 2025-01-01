1857d6a46019fef3a2c2523929b1a44e7e0d2027
Sven Johan Lindvall <sjlindvall@live.com>
2025-01-01 21:25:18 +0100

    Telnet POCs

---
+++ sbcmonsrv.h
@@ -0,0 +1,222 @@
+#ifndef SBCMONSRV_H
+#define SBCMONSRV_H
+
+
+#define STRSIZE 256
+#define INVALID -1
+#define PORT 23
+#define BUFSIZE 1024
+
+typedef enum
+{
+    SRVST_ERROR = 0,
+    SRVST_INITED,
+    SRVST_LISTENING,
+    SRVST_CLIENT_CONNECTED,
+    SRVST_MENU_SBCMON,
+    SRVST_MENU_PBI,
+    SRVST_REQUEST_IDF,
+    SRVST_RESPONS_IDF,
+    SRVST_DISCONNECTED
+} ServiceStates;
+
+typedef struct 
+{
+    FILE * thisFile;
+    char filename[STRSIZE];
+    char ipAddress[16];
+    int ipPort;
+    int numberCount;
+    ServiceStates state;
+} Settings;
+
+typedef struct
+{
+    uint8_t slot;
+    uint16_t idf;
+    uint16_t hex;
+} IdfReg;
+
+typedef struct
+{
+    uint8_t slot;
+    uint16_t modid;
+    char typename[16];
+    char partnum[16];
+    char serialnum[16];
+    char hwrev[8];
+    char swrev[8];
+} ModInfo;
+
+typedef struct 
+{
+    ModInfo  modinfo;
+    IdfReg idfreg[256];
+    int index;
+} Database;
+
+// Define the callback function type
+typedef void (*CallbackType)(const char *message);
+
+// Callback function that prints a message
+void printMessage(const char *message) 
+{
+    printf("Message: %s\n", message);
+}
+
+// Another callback function that prints a different format
+void printFormattedMessage(const char *message) 
+{
+    printf("Formatted Message: --> %s <--\n", message);
+}
+
+// Function that takes a callback and a message, and calls the callback
+void performAction(CallbackType callback, const char *message) 
+{
+    callback(message);
+}
+
+void openFile(const char *filename, Settings *settings)
+{
+    strcpy(settings->filename, filename);
+    settings->thisFile = fopen(settings->filename, "r");
+    settings->state = (settings->thisFile != 0) ? SRVST_INITED : SRVST_ERROR;
+}
+
+void setIpAddr(const char *ipaddr, Settings *settings)
+{
+    strcpy(settings->ipAddress, ipaddr);
+}
+
+void interpretArgs(int cnt, char*  vals[], Settings *sets)
+{
+    if (cnt == 1)
+    {
+        printf("%s args:\n\
+  -f <filename>\n\
+  -ip <ip address>\n\
+  -p <ip port>\n\
+  -c <scan count>\n", vals[0]);
+    }
+    else
+    {    
+        //printf("Arguments:\n-------------------------\n");
+        for (int n=1; n < cnt; n++)
+        {
+            //printf("%u : %s\n", n, vals[n]);
+            if (n < cnt && strstr(vals[n],"-f") > 0)
+            {
+                n++;
+                openFile(vals[n], sets);
+            }
+            else if (n < cnt && strstr(vals[n],"-ip") > 0)
+            {
+                n++;
+                setIpAddr(vals[n], sets);
+            }    
+            else if (n < cnt && strstr(vals[n],"-p") > 0)
+            {
+                n++;
+                sets->ipPort = strtol(vals[n], NULL, 10);
+            }    
+            else if (n < cnt && strstr(vals[n],"-c") > 0)
+            {
+                n++;
+                sets->numberCount = strtol(vals[n], NULL,10);
+            }
+            else
+            {
+                printf("invalid argument ! %s\n", vals[n]);
+            }
+        }
+    }
+}
+
+
+void printSettings(Settings *settings)
+{
+    printf("Settings:\n-------------------------\n\
+ File       : %s\n\
+ IP address : %s\n\
+ IP port    : %d\n\
+ Count      : %d\n\
+ State      : %d\n", 
+   settings->filename,
+   settings->ipAddress,
+   settings->ipPort,
+   settings->numberCount,
+   settings->state);
+    printf("\n");
+}
+
+void readFile(Settings *flg, Database *db)
+{
+    char oneLine[STRSIZE];
+    char *thisToken;
+    char *rest, *more;
+    char *firstArg, *secondArg, *thirdArg, *endp;
+    unsigned int temp = 0;
+    db->index = 0;
+    // read and tokenize a line from open file
+    while (fgets(oneLine, STRSIZE, flg->thisFile ) > 0)
+    {
+        thisToken = strtok_r(oneLine, "\t", &rest);
+    // interpret first token,
+        if (strstr(thisToken, "#") > 0)
+        {
+    // discard and do next line if token starts with '#'
+            continue;
+        }    
+        else if (thisToken != NULL && strstr(thisToken,"idf") > 0)
+        {
+    // and treat rest of tokens accordingly.
+            firstArg = strtok_r(rest, "\t", &more);
+            if (firstArg != NULL && (temp = strtol(firstArg, &endp, 10)) > 0)
+            {
+                //printf("temp1=%x\n", temp);
+                db->idfreg[db->index].slot = (uint8_t)(temp);
+                secondArg = strtok_r(more, "\t", &rest);
+                if (secondArg != NULL && (temp = strtol(secondArg, &endp, 16)) > 0)
+                {
+                    //printf("temp2=%x\n", temp);
+                    db->idfreg[db->index].idf = (uint16_t)(temp);
+                    thirdArg = strtok_r(rest, "\t", &more);
+                    if (thirdArg != NULL && (temp = strtol(thirdArg, &endp, 16)) > 0)
+                    {
+                        //printf("temp3=%x\n", temp);
+                        db->idfreg[db->index].hex = (uint16_t)(temp);
+                        db->index++;
+                    }
+                }
+            }
+        }
+    }            
+}
+
+void listDb(Database *db)
+{
+    printf("DB listing:\nSlot\tIDF\tHex\n---------------------\n");    
+    for (int i = 0; i < db->index; i++)
+    {
+        printf("%d\t%x\t%x\n", db->idfreg[i].slot,db->idfreg[i].idf,db->idfreg[i].hex);
+    }
+    printf("\n");
+}
+
+
+int lkupIdfReg(Database *db, uint8_t slot, uint16_t idf)
+{
+    int result;
+    for (int i = 0; i < db->index; i++)
+    {
+        // search for index
+        if (db->idfreg[i].slot == slot && db->idfreg[i].idf == idf)
+        {
+            result = (int)(db->idfreg[i].hex);
+            return result;
+        }
+    }
+    return -1;
+}
+
+#endif
---
+++ telnetPosixServer.c
@@ -0,0 +1,245 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include "sbcmonsrv.h"
+
+
+Settings globals;
+Database database;
+
+// Define the callback function type
+typedef void (*CallbackType)(const char *message);
+
+void handleArguments(int cnt, char*  vals[], Settings *sets)
+{
+    if (cnt == 1)
+    {
+        printf("%s args:\n\
+  -f <filename>\n\
+  -ip <ip address>\n\
+  -p <ip port>\n\
+  -c <scan count>\n", vals[0]);
+    }
+    else
+    {    
+        printf("Arguments:\n-------------------------\n");
+        for (int n=1; n < cnt; n++)
+        {
+            printf("%u : %s\n", n, vals[n]);
+            if (n < cnt && strstr(vals[n],"-f") > 0)
+            {
+                n++;
+                openFile(vals[n], sets);
+            }
+            else if (n < cnt && strstr(vals[n],"-ip") > 0)
+            {
+                n++;
+                setIpAddr(vals[n], sets);
+            }    
+            else if (n < cnt && strstr(vals[n],"-p") > 0)
+            {
+                n++;
+                sets->ipPort = strtol(vals[n], NULL, 10);
+            }    
+            else if (n < cnt && strstr(vals[n],"-c") > 0)
+            {
+                n++;
+                sets->numberCount = strtol(vals[n], NULL,10);
+            }
+            else
+            {
+                printf("invalid argument ! %s\n", vals[n]);
+            }
+        }
+    }
+}
+
+
+typedef void (*ServiceCbt)(int argc, const char * args[]);
+
+// Structure to hold the data to be passed to the thread
+typedef struct {
+    int clientSocket;
+    pthread_mutex_t *mutex;
+    char buffer[BUFSIZE];
+    ServiceCbt service;
+    ServiceStates state;
+} ThreadData;
+
+
+// Function to send data to an open client socket
+void sendReply(int sock, const char *data) {
+    ssize_t bytesSent = send(sock, data, strlen(data), 0);
+    if (bytesSent == -1) {
+        printf("send failed: %s\n", strerror(errno));
+    } else {
+        printf("sendt data: %s\n", data);
+    }
+}
+
+void runConsole(ThreadData *context)
+{
+    printf("[%d]received data: %s\n", context->state, context->buffer);
+    ServiceStates st = context->state;
+    
+    switch (st) {
+
+        SRVST_CLIENT_CONNECTED:
+            if (strstr(context->buffer,"sbcmon")>0)
+            {
+                context->state = SRVST_MENU_SBCMON;
+                sendReply(context->clientSocket, "Test program RCU bla bla\n\nSBCMON:");
+            }
+            printf("SRVST_CLIENT_CONNECTED\n");
+            break;
+
+        SRVST_MENU_SBCMON:
+            if (strstr(context->buffer,"q")>0)
+            {
+                context->state = SRVST_CLIENT_CONNECTED;
+                sendReply(context->clientSocket, "->");
+            }
+            else  if (strstr(context->buffer,"pbi")>0)
+            {
+                context->state = SRVST_MENU_PBI;
+                printf("SRVST_MENU_PBI\n");
+                sendReply(context->clientSocket, "SBCMON/PBI:");
+            }
+            printf("SRVST_MENU_SBCMON\n");
+            break;
+
+        SRVST_MENU_PBI:
+            if (strstr(context->buffer,"q")>0)
+            {
+                context->state = SRVST_MENU_SBCMON;
+                sendReply(context->clientSocket, "SBCMON:");
+            }
+            else  if (strstr(context->buffer,"rdi")>0)
+            {
+                context->state = SRVST_REQUEST_IDF;
+                printf("SRVST_MENU_PBI\n");
+                sendReply(context->clientSocket, "SBCMON/PBI/RDI");
+            }
+            printf("SRVST_MENU_PBI\n");
+            break;
+
+
+        SRVST_ERROR:
+            break;
+        default:
+            break;
+    }
+
+
+
+    
+}
+
+
+// Thread function for handling client communication
+void *ClientHandler(void *arg) {
+    ThreadData *data = (ThreadData *)arg;
+    int clientSocket = data->clientSocket;
+    ssize_t bytesReceived;
+
+    data->state =  SRVST_CLIENT_CONNECTED;
+    sendReply(clientSocket, "->");
+    while ((bytesReceived = recv(clientSocket, data->buffer, BUFSIZE, 0)) > 0) {
+        data->buffer[bytesReceived] = '\0';
+        pthread_mutex_lock(data->mutex);
+        // Do some stuff with the received data
+        runConsole(data);
+        pthread_mutex_unlock(data->mutex);
+    }
+
+    close(clientSocket);
+    free(data);
+    return NULL;
+}
+
+// Event handler function
+void EventHandler(pthread_mutex_t *mutex, ThreadData *data) {
+    char line[BUFSIZE];
+
+    while (1) {
+        pthread_mutex_lock(mutex);
+        if (strlen(line) > 0) {
+            if (strstr(line, "noe") == NULL) {
+                sendReply(data->clientSocket, "noe 1234  5678 FFFF");
+            } else {
+                printf("Echo data: %s\n", line);
+                sendReply(data->clientSocket, line);  // Send data to open client socket
+            }
+            strcpy(line, "");
+        }
+        pthread_mutex_unlock(mutex);
+        // Sleep for a short period to prevent busy-waiting
+        usleep(100000);  // 100 milliseconds
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    int listenSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
+    pthread_t thread;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    handleArguments(argc, argv, &globals);
+    printSettings(&globals);
+    if (globals.state == SRVST_ERROR) exit(1);
+
+    listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == -1) {
+        printf("Socket creation failed: %s\n", strerror(errno));
+        return 1;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons((globals.ipPort > 0) ? globals.ipPort : PORT);
+
+    if (bind(listenSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        printf("Bind failed: %s\n", strerror(errno));
+        close(listenSocket);
+        return 1;
+    }
+
+    if (listen(listenSocket, SOMAXCONN) == -1) {
+        printf("Listen failed: %s\n", strerror(errno));
+        close(listenSocket);
+        return 1;
+    }
+
+    printf("Listening for connections on port %d...\n", ntohs(serverAddr.sin_port));
+
+    while ((clientSocket = accept(listenSocket, (struct sockaddr *)&clientAddr, &addrLen)) != -1) {                        
+        printf("Client %s connected.\n", inet_ntoa(clientAddr.sin_addr));
+
+        ThreadData *data = (ThreadData *)malloc(sizeof(ThreadData));
+        data->clientSocket = clientSocket;
+        data->mutex = &mutex;
+
+        // Create thread to handle client communication
+        if (pthread_create(&thread, NULL, ClientHandler, data) != 0) {
+            printf("Thread creation failed: %s\n", strerror(errno));
+            close(clientSocket);
+            free(data);
+        } else {
+            pthread_detach(thread);  // Detach thread to automatically clean up resources
+        }
+    }
+
+    close(listenSocket);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
